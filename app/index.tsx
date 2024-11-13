7b443a70b485308c0736f7d93a7cbda1dab510de
Guiga00 <guilhermemedeiros01844@gmail.com>
2024-11-12 22:55:21 -0300

    Tela de configurações, falta colocar os icones

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import { Text, View } from "react-native";
+import Configuracoes from "./screens/configuracoes";
+import React from "react";
 
 export default function Index() {
   return (
@@ -9,7 +11,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Configuracoes/>
     </View>
   );
 }
