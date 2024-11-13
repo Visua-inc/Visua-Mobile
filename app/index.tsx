79e9c89156656e7ced4a500ab96d2d950af6ff5f
Miguel Ribeiro <155123087+Leugz@users.noreply.github.com>
2024-11-13 07:32:29 -0300

    layout finished

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter()
+
   return (
     <View
       style={{
@@ -10,6 +13,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="Layout" onPress={() => router.push('/layout')} />
     </View>
   );
 }
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,6 @@
+import { Ionicons } from "@expo/vector-icons";
 import React, { ReactNode } from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import { Text, View, Button, StyleSheet, Pressable } from "react-native";
 
 interface LayoutProps {
   children: ReactNode
@@ -8,14 +9,21 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.text}>Texto</Text>
+      <View style={styles.upContainer}>
+        <Text style={styles.text}>DESCRITOR DE OBJETO. APONTE A CÂMERA A DOIS PALMOS DO OBJETO E SOARÁ UM BIP QUANDO ELE FOR LIDO.</Text>
       </View>
       {children}
       <View style={styles.buttonContainer}>
-        <Button title="Botão 1" onPress={() => {}} />
-        <Button title="Botão 2" onPress={() => {}} />
-        <Button title="Botão 3" onPress={() => {}} />
+      <Pressable>
+          <Ionicons name="list" size={72} color="white" />
+        </Pressable>
+        <Pressable>
+          <Ionicons name="barcode-sharp" size={72} color="white" />
+        </Pressable>
+        <Pressable>
+          <Ionicons name="camera" size={72} color="white" />
+        </Pressable>
+        
       </View>
     </View>
   );
@@ -26,29 +34,44 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     alignItems: "center",
-    padding: 20, // Adicionando um pouco de espaço para o layout
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
+    color: "white",
+  },
+  upContainer: {
+    width: "100%",
+
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+
+    paddingVertical: 40,
+
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+
+    backgroundColor: "#1C7396",
   },
   buttonContainer: {
     width: "100%", // Tornando os botões responsivos
 
     display: "flex",
     flexDirection: "row",
-
-    padding: ""
+    flexWrap: "wrap",
 
     alignItems: "center",
-    gap: 60,
+    justifyContent: "space-evenly",
+
+    paddingVertical: 30,
+
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+
+    position: "absolute",
+    bottom: 0,
 
     backgroundColor: "#1C7396",
-  },
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
-    marginTop: 20,
   },
 });
