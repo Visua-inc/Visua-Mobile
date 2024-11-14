b840079ddb57d09df08fb22ecce987cb53427e98
Miguel Ribeiro <155123087+Leugz@users.noreply.github.com>
2024-11-13 22:09:33 -0300

    NÃO É POSSIVEL

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,6 @@
-import { useRouter } from 'expo-router'
-import { Button, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default function Index() {
-  const router = useRouter()
-
   return (
     <View
       style={{
